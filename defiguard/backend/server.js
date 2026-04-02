8ed65bf5277e4800c827ae7a6cd41ef7cec86330
heeeevi <mbohwes0303@gmail.com>
2026-04-02 08:30:28 +0700

    Fix Express wildcard route

--- defiguard/backend/server.js
+++ defiguard/backend/server.js
@@ -39,7 +39,7 @@ app.post('/api/chat', (req, res) => {
 });
 
 // Catch-all route to serve the React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
